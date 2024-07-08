88b67f5dbaa05c9d91e1fe5892d1d13dcd398b4c
r4g309 <bscortes94@ucatolica.edu.co>
2024-07-08 13:10:29 -0500

    Initial dependencies

---
+++ src/test/java/dev/r4g309/foroalura/ForoAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.r4g309.foroalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoAluraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
